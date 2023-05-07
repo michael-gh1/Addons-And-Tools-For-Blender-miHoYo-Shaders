f1babed5b23a2d7d61f23e731b1398a1ffd90bc7
Michael <8632035+michael-gh1@users.noreply.github.com>
2023-05-07 00:47:49 -0700

    tidy: remove dead code (see GenshinTextureImporter)

--- setup_wizard/genshin_import_textures.py
+++ setup_wizard/genshin_import_textures.py
@@ -9,10 +9,8 @@ from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty
 from bpy.types import Operator
 
-from setup_wizard.texture_setup.game_texture_importers import GameTextureImporterFactory, GameTextureImporterType, GenshinImpactTextureImporter
-
-from setup_wizard.import_order import get_actual_material_name_for_dress
 from setup_wizard.models import CustomOperatorProperties
+from setup_wizard.texture_setup.game_texture_importers import GameTextureImporterFactory, GameTextureImporterType, GenshinImpactTextureImporter
 
 
 class GI_OT_GenshinImportTextures(Operator, ImportHelper, CustomOperatorProperties):
@@ -43,59 +41,6 @@ class GI_OT_GenshinImportTextures(Operator, ImportHelper, CustomOperatorProperti
 
         super().clear_custom_properties()
         return {'FINISHED'}
-
-    '''
-    Deprecated: No longer needed after shader rewrite because normal map is plugged by default
-    Still maintains backward compatibility by only trying this if `label_name` is found in the node tree.
-    '''
-    def plug_normal_map(self, shader_material_name, label_name):
-        shader_group_material_name = 'Group.001'
-        shader_material = bpy.data.materials.get(shader_material_name)
-
-        if shader_material:
-            normal_map_node_color_outputs = [node.outputs.get('Color') for node in shader_material.node_tree.nodes \
-                if node.label == label_name and not node.outputs.get('Color').is_linked]
-            
-            if normal_map_node_color_outputs:
-                normal_map_node_color_output = normal_map_node_color_outputs[0]
-                normal_map_input = shader_material.node_tree.nodes.get(shader_group_material_name).inputs.get('Normal Map')
-
-                bpy.data.materials.get(shader_material_name).node_tree.links.new(
-                    normal_map_node_color_output,
-                    normal_map_input
-                )
-    
-    def setup_dress_textures(self, texture_name, texture_img):
-        shader_dress_materials = [material for material in bpy.data.materials if 'Genshin Dress' in material.name]
-        shader_cloak_materials = [material for material in bpy.data.materials
-                                  if 'Genshin Arm' in material.name or 'Genshin Cloak' in material.name]
-
-        # TODO: Refactor this for sure!
-        # Specific case for Xiao (the only character with an Arm material)
-        # Specific case for Dainsleif (the only character with a Cloak material)
-        # Technically Paimon has one, but we ignore it
-        if shader_cloak_materials:
-            original_cloak_material = [material for material in bpy.data.materials if material.name.endswith(
-                shader_cloak_materials[0].name.split(' ')[-1]
-            )][0]  # the material that ends with 'Dress', 'Dress1', 'Dress2'
-            actual_cloak_material = get_actual_material_name_for_dress(original_cloak_material.name)
-            if actual_cloak_material in texture_name:
-                material_shader_nodes = bpy.data.materials.get(shader_cloak_materials[0].name).node_tree.nodes
-                material_shader_nodes.get(f'{texture_name}_UV0').image = texture_img
-                material_shader_nodes.get(f'{texture_name}_UV1').image = texture_img
-
-        for shader_dress_material in shader_dress_materials:
-            original_dress_material = [material for material in bpy.data.materials if material.name.endswith(
-                shader_dress_material.name.split(' ')[-1]
-            )][0]  # the material that ends with 'Dress', 'Dress1', 'Dress2'
-
-            actual_material = get_actual_material_name_for_dress(original_dress_material.name)
-            if actual_material in texture_name:
-                self.report({'INFO'}, f'Importing texture "{texture_name}" onto material "{shader_dress_material.name}"')
-                material_shader_nodes = bpy.data.materials.get(shader_dress_material.name).node_tree.nodes
-                material_shader_nodes.get(f'{texture_name}_UV0').image = texture_img
-                material_shader_nodes.get(f'{texture_name}_UV1').image = texture_img
-                return
 
 
 register, unregister = bpy.utils.register_classes_factory(GI_OT_GenshinImportTextures)
