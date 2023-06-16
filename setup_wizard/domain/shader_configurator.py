5de819b0ef33370540bcd4868bfe1e676011dfe9
Michael <8632035+michael-gh1@users.noreply.github.com>
2023-06-16 03:56:26 -0700

    refactor: check if not material earlier, reduce mental overhead

--- setup_wizard/domain/shader_configurator.py
+++ setup_wizard/domain/shader_configurator.py
@@ -7,15 +7,16 @@ class ShaderConfigurator:
 
     def update_shader_value(self, materials, node_name, input_name, value):
         for material in materials:
-            if material:
-                internal_node_name = self.v2_node_name_mapping.get(node_name) if \
-                    self.v2_node_name_mapping.get(node_name) else self.v1_node_name_mapping.get(node_name)
+            if not material:
+                continue
+            internal_node_name = self.v2_node_name_mapping.get(node_name) if \
+                self.v2_node_name_mapping.get(node_name) else self.v1_node_name_mapping.get(node_name)
 
-                shader_node = material.node_tree.nodes.get(internal_node_name)
-                shader_node_inputs = shader_node.inputs if shader_node else None
+            shader_node = material.node_tree.nodes.get(internal_node_name)
+            shader_node_inputs = shader_node.inputs if shader_node else None
 
-                if shader_node_inputs:
-                    shader_node_input = shader_node_inputs.get(input_name)
+            if shader_node_inputs:
+                shader_node_input = shader_node_inputs.get(input_name)
 
-                    if shader_node_input:
-                        shader_node_input.default_value = value
+                if shader_node_input:
+                    shader_node_input.default_value = value
