1d8cca7f2a6bb8c2797442d32bb5d1a0b9f33024
Michael <8632035+michael-gh1@users.noreply.github.com>
2023-10-22 17:22:26 -0700

    fix: Head Origin (v3) not being set up like Head Driver (v2)

--- setup_wizard/set_up_head_driver.py
+++ setup_wizard/set_up_head_driver.py
@@ -3,33 +3,28 @@
 import bpy
 from bpy.types import Operator
 
-from setup_wizard.domain.shader_identifier_service import GenshinImpactShaders, ShaderIdentifierServiceFactory
 from setup_wizard.import_order import NextStepInvoker
 from setup_wizard.setup_wizard_operator_base_classes import CustomOperatorProperties
 
 HEAD_DRIVER_OBJECT_NAME = 'Head Driver'
+HEAD_ORIGIN_OBJECT_NAME = 'Head Origin'
 
 
 class GI_OT_SetUpHeadDriver(Operator, CustomOperatorProperties):
     '''Sets up Head Driver'''
     bl_idname = 'genshin.setup_head_driver'
-    bl_label = 'Genshin: Setup HeadDriver'
+    bl_label = 'Genshin: Setup Head Driver'
 
     def execute(self, context):
-        shader_identifier_service = ShaderIdentifierServiceFactory.create(self.game_type)
-        shader_version = shader_identifier_service.identify_shader(bpy.data.materials, bpy.data.node_groups)
+        head_driver_object = bpy.data.objects.get(HEAD_DRIVER_OBJECT_NAME) or bpy.data.objects.get(HEAD_ORIGIN_OBJECT_NAME)
+        child_of_constraint = head_driver_object.constraints[0]  # expecting 1 constraint head driver
 
-        if shader_version is GenshinImpactShaders.V1_GENSHIN_IMPACT_SHADER or \
-            shader_version is GenshinImpactShaders.V2_GENSHIN_IMPACT_SHADER:
-            head_driver_object = bpy.data.objects.get(HEAD_DRIVER_OBJECT_NAME)
-            child_of_constraint = head_driver_object.constraints[0]  # expecting 1 constraint head driver
+        armature = [object for object in bpy.data.objects if object.type == 'ARMATURE'][0]  # expecting 1 armature
+        armature_bones = armature.data.bones
+        head_bone_name = [bone_name for bone_name in armature_bones.keys() if 'Head' in bone_name][0]  # expecting 1 bone with Head in the name
 
-            armature = [object for object in bpy.data.objects if object.type == 'ARMATURE'][0]  # expecting 1 armature
-            armature_bones = armature.data.bones
-            head_bone_name = [bone_name for bone_name in armature_bones.keys() if 'Head' in bone_name][0]  # expecting 1 bone with Head in the name
-
-            self.set_contraint_target_and_bone(child_of_constraint, armature, head_bone_name)
-            self.set_inverse(head_driver_object)
+        self.set_contraint_target_and_bone(child_of_constraint, armature, head_bone_name)
+        self.set_inverse(head_driver_object)
 
         if self.next_step_idx:
             NextStepInvoker().invoke(
