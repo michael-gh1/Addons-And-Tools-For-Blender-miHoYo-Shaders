7a98aa2f7b74e15c563fa95c2b0ed7a0b533896e
Michael <8632035+michael-gh1@users.noreply.github.com>
2023-12-12 00:33:59 -0800

    fix: ClothOL material not having texture by reusing Cloth material

--- setup_wizard/replace_default_materials_setup/game_default_material_replacers.py
+++ setup_wizard/replace_default_materials_setup/game_default_material_replacers.py
@@ -326,6 +326,8 @@ class PunishingGrayRavenDefaultMaterialReplacer(GameDefaultMaterialReplacer):
                     mesh_body_part_name
                 if 'Face' in mesh_body_part_name:  # 6.Karenina_Ember (material w/ Face in it, but no called just Face)
                     mesh_body_part_name = 'Face'
+                elif 'ClothOL' in mesh_body_part_name:
+                    mesh_body_part_name = 'Cloth'
 
                 if mesh_body_part_name and 'Alpha' not in mesh_body_part_name:
                     material_type = JaredNytsPunishingGrayRavenShaderMaterialNames.HAIR if 'Hair' in mesh_body_part_name else \
