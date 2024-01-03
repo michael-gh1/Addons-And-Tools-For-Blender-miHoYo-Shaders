202881b04c20339e520c04bb0bdbaf66a3c3904b
Michael <8632035+michael-gh1@users.noreply.github.com>
2024-01-03 00:33:53 -0800

    fix: Furina (Default) inconsistent EffectHair material name causes error (#80)

--- setup_wizard/replace_default_materials_setup/game_default_material_replacers.py
+++ setup_wizard/replace_default_materials_setup/game_default_material_replacers.py
@@ -58,6 +58,11 @@ class GenshinImpactDefaultMaterialReplacer(GameDefaultMaterialReplacer):
                 mesh_body_part_name = None
                 character_type = None
 
+                # Handle Avatar Furina, which has the material inconsistently named (ends w/ Effect, not EffectHair)
+                if mesh.name == 'EffectHair' and material_name == 'Avatar_Girl_Sword_Furina_Mat_Effect':
+                    material_slot.material.name = 'Avatar_Girl_Sword_Furina_Mat_EffectHair'
+                    material_name = material_slot.name
+
                 if material_name.startswith('NPC'):
                     mesh_body_part_name = self.__get_npc_mesh_body_part_name(material_name)
                     character_type = TextureImporterType.NPC
