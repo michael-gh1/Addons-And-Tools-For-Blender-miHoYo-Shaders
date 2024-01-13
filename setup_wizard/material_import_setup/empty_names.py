08acd705840a6d27baaeb232a951522f3d31bde6
Michael <8632035+michael-gh1@users.noreply.github.com>
2024-01-13 15:06:12 -0800

    feat: append empties used for Light Vectors if missing - scenario: Blender 4.0 uses shader saved in Blender 3.6 - empties used in Light Vectors are not getting automatically appended

---
+++ setup_wizard/material_import_setup/empty_names.py
@@ -0,0 +1,22 @@
+# Author: michael-gh1
+
+
+class EmptyNames:
+    HEAD_ORIGIN = 'Head Origin'
+    HEAD_FORWARD = 'Head Forward'
+    HEAD_UP = 'Head Up'
+    LIGHT_DIRECTION = 'Light Direction'
+
+    LIGHT_DIRECTION_EMPTIES = [
+        HEAD_ORIGIN,
+        HEAD_FORWARD,
+        HEAD_UP,
+        LIGHT_DIRECTION
+    ]
+
+    LIGHT_DIRECTION_EMPTIES_FILE_IMPORT = [
+        {'name': HEAD_ORIGIN},
+        {'name': HEAD_FORWARD},
+        {'name': HEAD_UP},
+        {'name': LIGHT_DIRECTION},
+    ]
