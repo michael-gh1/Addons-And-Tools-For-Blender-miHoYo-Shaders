5f99556e47f8fccf2342206b7c12e46276727d7d
Michael <8632035+michael-gh1@users.noreply.github.com>
2024-10-21 22:54:55 -0700

    fix: StarCloakTypes enum values, off-by-one error

--- setup_wizard/domain/star_cloak_types.py
+++ setup_wizard/domain/star_cloak_types.py
@@ -6,7 +6,7 @@ class StarCloakTypes(Enum):
     The following StarCloak types MUST remain in the same order as in the shader.
     The value set here is used to set the value in the VFX shader node input.
     '''
-    PAIMON = 0
-    DAINSLIEF = 1
-    SKIRK = 2
-    ASMODAY = 3
+    PAIMON = 1
+    DAINSLIEF = 2
+    SKIRK = 3
+    ASMODAY = 4
