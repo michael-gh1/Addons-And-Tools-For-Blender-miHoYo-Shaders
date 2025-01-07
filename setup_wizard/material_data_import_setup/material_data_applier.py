07c0fa1ab017765fe73db6979fd40a7fe3577795
Michael <8632035+michael-gh1@users.noreply.github.com>
2025-01-07 14:24:24 -0800

    fix: one-off difference for Int between Shader and Material Data values

--- setup_wizard/material_data_import_setup/material_data_applier.py
+++ setup_wizard/material_data_import_setup/material_data_applier.py
@@ -596,7 +596,7 @@ class V4_MaterialDataApplier(V3_MaterialDataApplier):
             material_json_value = bool(material_json_value)
         elif (type(input_object) is bpy.types.NodeSocketInt or type(input_object) is bpy.types.NodeTreeInterfaceSocketInt) and \
             type(material_json_value) is float:
-            material_json_value = int(material_json_value)
+            material_json_value = int(material_json_value) + 1  # Shader values are one-based while material data is zero-based indexing
         elif self.is_number_of_values_mismatch(input_object, material_json_value):
             material_json_value = material_json_value[:3]
         return material_json_value
