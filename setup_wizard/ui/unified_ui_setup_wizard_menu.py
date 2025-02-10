5b9f8bdfd5633a85cc5a23f0ba5bc46ba3ff4816
Michael <8632035+michael-gh1@users.noreply.github.com>
2025-02-09 23:42:57 -0800

    feat: remove unused settings (moved to preferences.py)

--- setup_wizard/ui/unified_ui_setup_wizard_menu.py
+++ setup_wizard/ui/unified_ui_setup_wizard_menu.py
@@ -26,46 +26,6 @@ class CSW_PT_Unified_Character_Setup_Wizard_UI_Layout(Panel):
         default=GameType.GENSHIN_IMPACT.name,
     )
 
-    # Addon updater preferences.
-    auto_check_update: bpy.props.BoolProperty(
-        name='Auto-check for Update',
-        description='If enabled, auto-check for updates using an interval',
-        default=False
-    )
-
-    auto_check_update: bpy.props.BoolProperty(
-        name="Auto-check for Update",
-        description="If enabled, auto-check for updates using an interval",
-        default=False)
-
-    updater_interval_months: bpy.props.IntProperty(
-        name='Months',
-        description="Number of months between checking for updates",
-        default=0,
-        min=0)
-
-
-    updater_interval_days: bpy.props.IntProperty(
-        name='Days',
-        description="Number of days between checking for updates",
-        default=7,
-        min=0,
-        max=31)
-
-    updater_interval_hours: bpy.props.IntProperty(
-        name='Hours',
-        description="Number of hours between checking for updates",
-        default=0,
-        min=0,
-        max=23)
-
-    updater_interval_minutes: bpy.props.IntProperty(
-        name='Minutes',
-        description="Number of minutes between checking for updates",
-        default=0,
-        min=0,
-        max=59)
-
     def draw(self, context):
         layout = self.layout
 
